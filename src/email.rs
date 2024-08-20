ee26479b79602bd9a9904e85fdff670324d5e247
Guillemin Maxime <mguillemin@innlog.fr>
2024-08-20 17:19:27 +0200

    feat: vérification si adresse email est en min

--- src/email.rs
+++ src/email.rs
@@ -1,5 +1,9 @@
 use regex::Regex;
 
+pub fn is_lowercase(email: &str) -> bool {
+    email == email.to_lowercase()
+}
+
 pub fn is_valid_email(email: &str) -> bool {
     let email_regex = Regex::new(
         r"(?i)^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$",
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,20 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        eprintln!("Usage: {} <email>", args[0]);
+        eprintln!("Utilisation: {} <email>", args[0]);
         process::exit(1);
     }
 
     let email = &args[1];
-    
+
+    if !email::is_lowercase(email) {
+        println!("'{}' n'est pas entièrement en minuscule.", email);
+        process::exit(1);
+    }
+
     if email::is_valid_email(email) {
-        println!("'{}' is a valid email address.", email);
+        println!("'{}' est une adresse mail valide.", email);
     } else {
-        println!("'{}' is not a valid email address.", email);
+        println!("'{}' n'est pas une adresse mail valide.", email);
     }
 }
